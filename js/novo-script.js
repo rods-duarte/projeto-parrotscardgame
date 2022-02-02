a82052996931a5ebd1312fa0408e8843b951a681
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2022-02-02 15:02:44 -0300

    backup

---
+++ js/novo-script.js
@@ -0,0 +1,1 @@
+let cartas = document.querySelectorAll(`.carta`);
--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 let cartas = [1,1,2,2,3,3,4,4,5,5,6,6,7,7];
 let numeroDeCartas;
 let contador = 0; // conta quantas cartas foram acertadas
+let jogadas = 0;
 
-// verifica quantas cartas serao jogadas
-while(numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 != 0) {  
-    numeroDeCartas = prompt(`Insira com quantas cartas quer jogar (deve ser entre 4 e 14)`);
-}  
-
-prepararJogo(cartas);
+function main() {
+    // verifica quantas cartas serao jogadas
+    while(numeroDeCartas < 4 || numeroDeCartas > 14 || numeroDeCartas % 2 != 0) {  
+        numeroDeCartas = prompt(`Insira com quantas cartas quer jogar (deve ser entre 4 e 14)`);
+    }  
+    prepararJogo(cartas);
+}
 
 // ajusta o numero de cartas e sorteia a posicao delas
 function sortearArray(arr) {  
@@ -52,6 +54,8 @@ function clicarCarta(carta) {
     } else {  // Se a carta selecionada for a primeira
         carta.classList.add(`selecionado`);
     }
+
+    jogadas++;
 }
 
 // Compara se duas cartas sao iguais 
@@ -61,7 +65,7 @@ function compararCartas(carta, cartaSelecionado) {
         contador += 2;
         console.log(contador);
         console.log(numeroDeCartas);
-        if(contador == numeroDeCartas) {alert(`Fim de jogo`)};  // Condicao quando jogo termina
+        if(contador == numeroDeCartas) {alert(`Voce ganhou em ${jogadas} jogadas !`)};  // Condicao quando jogo termina
     } else {  // Condicao quando cartas sao diferentes
         let frenteCarta1, versoCarta1, frenteCarta2, versoCarta2;
         [frenteCarta1, versoCarta1] = [carta.querySelector(`.frente`), carta.querySelector(`.verso`)];
@@ -80,3 +84,4 @@ function compararCartas(carta, cartaSelecionado) {
     }
 }
 
+main();
